e449eb2c3a775d5ee6eeab8befb2f904f9a7a186
Chamil José Cruz Razeq <chamil@Chamils-MacBook-Pro.home>
2022-11-10 23:59:01 +0000

    first commit

---
+++ solve_memoization.py
@@ -0,0 +1,20 @@
+# Recurrencia del problema del ladrón
+# -----------------------------------
+#    t(n) = max (t(n-2) + v[n], t(n-1))
+#    t(n) = 0		               : si n<0
+
+def solve_memoization(items):
+    mem = {}
+
+    # Utiliza esta función para programar el código de
+    # tu recurrencia utilizando memoization.
+    #   Aviso: Para resolver este ejercicio no es valido
+    #          utilizar el soporte de @functools
+    def t(n):
+        return 0
+
+    return 0
+# Recurrencia del problema del ladrón
+# -----------------------------------
+#    t(n) = max (t(n-2) + v[n], t(n-1))
+#    t(n) = 0		               : si n<0
---
+++ solve_tabulation.py
@@ -0,0 +1,15 @@
+def solve_tabulation(items):
+    table = []
+    
+    def fill_table():
+        # Rellena la tabla 'table' con las soluciones de todos los 
+        # subproblemas (o sea, los beneficios máximos que puede
+        # conseguir el ladrón)
+        # ...
+        return
+
+    fill_table()
+
+    max_benefit = table[-1]     # El máximo beneficio está en el
+                                # último elemento de la tabla
+    return max_benefit
